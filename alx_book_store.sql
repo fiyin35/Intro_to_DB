434a4ec6418bd788051001838e9e85c5ac7820eb
Fiyinfolu <lolafiyin@gmail.com>
2024-10-19 00:52:26 +0100

    add the sql statement

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,15 +25,15 @@ CREATE TABLE Customers (
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
-    oder_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 )
 
 CREATE TABLE Order_Details (
     order_detail_id INT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 )
 
