2e3613b57dae3dcbfa58d6d28bce4d06ce9ca10a
Fiyinfolu <lolafiyin@gmail.com>
2024-10-19 01:13:16 +0100

    add my sql server script to connect to database

---
+++ MySQLServer.py
@@ -0,0 +1,32 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+database = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+#connect to database
+
+def connectDB():
+    try:
+        mydb = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            password = 'mydummypassword',
+            database = database
+            )
+        mycursor = mydb.cursor()
+        print("Database 'alx_book_store' created successfully!")
+    except mysql.exceptions.ConnectionError as e:
+        print(f"Error connecting to database: {e}")
+    except Error as e:
+        print(e)
+    finally:
+        if mycursor:
+            mycursor.close()
+        if mydb:
+            mydb.close()
+    
+    
+
+
+print(f"Database {database} created successfully!")
+
